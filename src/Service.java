72641ede1940a0aa9143c7820e5b3e4c57a3b649
Star1725 <65925726+Star1725@users.noreply.github.com>
2025-01-24 18:45:31 +0300

    - After completion of the service1 execution, service2 is automatically started, which executes the callService2 method; - thenRunAsync ensures that callService2 is also executed on a separate thread from the shared ForkJoinPool; - service2 will not start until service1 is fully completed.; - this ensures strict sequential execution: first callService 1, then callService2.

--- src/Service.java
+++ src/Service.java
@@ -3,17 +3,28 @@ import java.util.concurrent.CompletableFuture;
 public class Service {
 
     public void executeServices(){
-        //run service 1
+        //service 1
         CompletableFuture<Void> service1 = CompletableFuture.runAsync(this::callService1);//Запускает Service 1 асинхронно в отдельном потоке с использованием CompletableFuture.runAsync.
         //Метод runAsync запускает переданную задачу (вызов callService1) в ForkJoinPool (дефолтный пул потоков в Java), позволяя выполнять её параллельно с основным потоком.
-        CompletableFuture.allOf(service1).join();//дожидается завершения service1 перед продолжением выполнения основного потока.
-        System.out.println("Main Thread: Service 1 is Completed");
+
+        //service 2
+        CompletableFuture<Void> service2 = service1.thenRunAsync(this::callService2);//После завершения выполнения service1 автоматически запускается service2, который выполняет метод callService2.
+
+        System.out.println("Main Thread: join services");
+        CompletableFuture.allOf(service1, service2).join();//дожидается завершения service1 перед продолжением выполнения основного потока.
+        System.out.println("Main Thread: all services completed");
     }
 
     private void callService1(){
         System.out.println("Service 1: Executing...");
-        workService(2000);
+        workService(3000);
         System.out.println("Service 1: Completed");
+    }
+
+    private void callService2() {
+        System.out.println("Service 2: Executing...");
+        workService(3000);
+        System.out.println("Service 2: Completed.");
     }
 
     private void workService(int t){
